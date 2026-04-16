6736dc4b74cdfb6db643fc0e3e7a74cfeef88754
Dhikshitha <ssdhikshitha@gmail.com>
2026-04-16 19:59:11 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const sections = document.querySelectorAll('section');
+
+const reveal = () => {
+  const trigger = window.innerHeight * 0.85;
+
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+
+    if (top < trigger) {
+      sec.classList.add('show');
+    }
+  });
+};
+
+window.addEventListener('scroll', reveal);
+reveal();
+
+
+
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.15
+});
+
+reveals.forEach(el => observer.observe(el));
