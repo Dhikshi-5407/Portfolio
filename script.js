282642ccaa1abcff2138cdec6de52d2562cbcc97
Dhikshitha <ssdhikshitha@gmail.com>
2026-04-17 21:34:11 +0530

    Portfolio deployment

--- script.js
+++ script.js
@@ -32,3 +32,11 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 reveals.forEach(el => observer.observe(el));
+
+const cards = document.querySelectorAll(".flip-card");
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("flip-mobile");
+  });
+});
