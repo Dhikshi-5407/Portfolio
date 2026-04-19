cce4ba3ea3912f993b9c95b51e40a10c28598b39
Dhikshitha <ssdhikshitha@gmail.com>
2026-04-19 21:14:27 +0530

    all

--- script.js
+++ script.js
@@ -1,44 +1,20 @@
-const form = document.getElementById("contact-form");
-const successMsg = document.getElementById("success-message");
-const button = form.querySelector("button");
+const sections = document.querySelectorAll('section');
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+    const reveal = () => {
+      const trigger = window.innerHeight * 0.85;
 
-  button.innerText = "Sending...";
-  button.disabled = true;
+      sections.forEach(sec => {
+        const top = sec.getBoundingClientRect().top;
 
-  const formData = new FormData(form);
+        if (top < trigger) {
+          sec.classList.add('show');
+        }
+      });
+    };
 
-  try {
-   const response = await fetch("https://dhikshitha.onrender.com/send-message", {
-  method: "POST",
-  body: formData
-});
+    window.addEventListener('scroll', reveal);
+    reveal();
 
-    // 🔥 SAFE JSON PARSE
-    let result = {};
-    try {
-      result = await response.json();
-    } catch {
-      console.warn("Response is not JSON");
-    }
 
-    if (response.ok) {
-      form.style.display = "none";
-      successMsg.style.display = "block";
-      form.reset();
-    } else {
-      alert(result.error || "Something went wrong ");
-      button.innerText = "Send Message";
-      button.disabled = false;
-    }
 
-  } catch (error) {
-    console.error("Fetch error:", error);
-    alert("Server not reachable ");
 
-    button.innerText = "Send Message";
-    button.disabled = false;
-  }
-});
